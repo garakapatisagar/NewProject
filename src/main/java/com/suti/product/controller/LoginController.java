6d69694142703ce950e7ef510b1da5d7df2647ba
garakapatisagar <garakapatisagar@gmail.com>
2026-08-08 19:27:02 +0530

    regitre msg chaned

--- src/main/java/com/suti/product/controller/LoginController.java
+++ src/main/java/com/suti/product/controller/LoginController.java
@@ -41,7 +41,7 @@ public class LoginController {
 	@PostMapping("/register")
 	public ResponseEntity<User> registerUser(@Valid @RequestBody RegisterUser user)
 	{
-		System.out.println("commitiing from local");
+		System.out.println("commitiing from local repositoty");
 		return new ResponseEntity<User>(service.createUser(user),HttpStatus.CREATED);
 	}
 	
